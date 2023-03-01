752726d254c7208a9e2ec069452e8dd5a8ce6fc8
Althea Janine Duran <112060534+Algessi@users.noreply.github.com>
2023-03-01 22:14:43 +0800

    Update script.js
    Added features:
    
    -Edit button to rename the task entered

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
-const input = document.querySelector('#todo-input');
-const addButton = document.querySelector('#add-button');
-const todoList = document.querySelector('#todo-list');
+const input = document.querySelector("#todo-input");
+const addButton = document.querySelector("#add-button");
+const todoList = document.querySelector("#todo-list");
 
-addButton.addEventListener('click', function() {
-  if (input.value === '') {
-    alert('Please enter a task!');
+addButton.addEventListener("click", function () {
+  if (input.value === "") {
+    alert("Please enter a task!");
     return;
   }
 
-  const newTodo = document.createElement('li');
-  newTodo.classList.add('todo-item');
+  const newTodo = document.createElement("li");
+  newTodo.classList.add("todo-item");
   newTodo.innerHTML = `
-    <span>${input.value}</span>
+    <span class="todo-name">${input.value}</span>
+    <button class="edit-button">Edit</button>
     <button class="delete-button">Delete</button>
   `;
   todoList.appendChild(newTodo);
-  input.value = '';
+  input.value = "";
 });
 
-input.addEventListener('keyup', function(event) {
+input.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
-    if (input.value === '') {
-      alert('Please enter a task!');
+    if (input.value === "") {
+      alert("Please enter a task!");
       return;
     }
-    
+
     addButton.click();
   }
 });
 
-todoList.addEventListener('click', function(event) {
-  if (event.target.classList.contains('delete-button')) {
+todoList.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete-button")) {
     event.target.parentElement.remove();
+  } else if (event.target.classList.contains("edit-button")) {
+    const todoName = event.target.parentElement.querySelector(".todo-name");
+    const todoInput = document.createElement("input");
+    todoInput.type = "text";
+    todoInput.value = todoName.textContent;
+    todoName.replaceWith(todoInput);
+
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "Save";
+    event.target.replaceWith(saveButton);
+
+    saveButton.addEventListener("click", function () {
+      todoName.textContent = todoInput.value;
+      todoInput.replaceWith(todoName);
+      saveButton.replaceWith(event.target);
+    });
   }
 });
-
-
